8a65bb2423bbf3d377d734f086299605d7102ba1
Camilo Mora <camilomorag2018@gmail.com>
2025-10-15 17:00:05 -0500

    Resueltos conflictos de merge en build.gradle y ReservaDTO

--- src/main/java/com/sistema_reservas/controller/dto/ReservaDTO.java
+++ src/main/java/com/sistema_reservas/controller/dto/ReservaDTO.java
@@ -10,14 +10,13 @@ public class ReservaDTO {
     private LocalDateTime fecha;
     private String estado;
 
-<<<<<<< HEAD
+
     // Getters y setterss
     public Long getIdUsuario() { return idUsuario; }
     public void setIdUsuario(Long idUsuario) { this.idUsuario = idUsuario; }
-=======
+
     public Long getId_reserva() { return id_reserva; }
     public void setId_reserva(Long id_reserva) { this.id_reserva = id_reserva; }
->>>>>>> 63e50ff4accfb5703f927565bd8dff0ba70df549
 
     public Long getUsuarioId() { return usuarioId; }
     public void setUsuarioId(Long usuarioId) { this.usuarioId = usuarioId; }
